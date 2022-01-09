2fdbb76252f974093138d87d846afa1e0da0eb21
Renaud Bédard <renaud.bedard@gmail.com>
2022-01-09 18:21:56 -0500

    Réinitialisation de l'état à chaque jour

--- src/lib/gameState.js
+++ src/lib/gameState.js
@@ -1,8 +1,27 @@
 import { browser } from '$app/env';
 import { writable } from 'svelte/store';
+
+let storedDate;
+if (browser) {
+  const storedDateString = localStorage.getItem('lastPlayedDate');
+  if (storedDateString) {
+    storedDate = new Date(storedDateString);
+
+    const today = new Date();
+    var todayString = '' + today.getUTCFullYear() + today.getUTCMonth() + today.getUTCDate();
+		var lastPlayedString = '' + storedDate.getUTCFullYear() + storedDate.getUTCMonth() + storedDate.getUTCDate();
+		if (todayString != lastPlayedString) {
+      console.log('A new day!');
+      storedDate = today;
+      window.localStorage.setItem('lastPlayedDate', storedDate.toISOString());
+			window.localStorage.setItem('inputState', null);
+      window.localStorage.setItem('rowState', null);
+		}
+  }
+}
  
-//export const lastPlayedDate = writable(browser ? new Date(localStorage.getItem('lastPlayedDate')) ?? new Date() : new Date());
-//lastPlayedDate.subscribe((value) => { if (browser) window.localStorage.setItem('lastPlayedDate', value.toISOString())});
+export const lastPlayedDate = writable(browser ? storedDate : new Date());
+lastPlayedDate.subscribe((value) => { if (browser) window.localStorage.setItem('lastPlayedDate', value == null ? null : value.toISOString())});
 
 export const inputState = writable(browser ? JSON.parse(localStorage.getItem('inputState')) ?? Array(5).fill('') : Array(5).fill(''));
 inputState.subscribe((value) => { if (browser) localStorage.setItem('inputState', JSON.stringify(value))});
